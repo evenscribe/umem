b0bac576c9573b6cb18ff75a408db5510de15068
Sangit Manandhar <rux12@icloud.com>
2025-06-11 19:41:27 -0400

    feat: rudimentary doc search setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-pub mod entities;
+use anyhow::Result;
+use umem_search::ProjectDirs;
+use umem_search::TraceIndex;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    TraceIndex::create_index(ProjectDirs::get_trace_index_path()?)?;
+    Ok(())
 }
