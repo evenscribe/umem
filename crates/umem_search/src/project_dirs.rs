1050aa4ae019320f23e02b97057d3b6df02f14a1
Sangit Manandhar <rux12@icloud.com>
2025-06-11 20:07:00 -0400

    fix: fix data dir path

--- crates/umem_search/src/project_dirs.rs
+++ crates/umem_search/src/project_dirs.rs
@@ -3,11 +3,12 @@ use std::path::PathBuf;
 
 pub struct ProjectDirs;
 
+const APP_PATH: &str = "umem";
 const TRACE_INDEX_PATH: &str = "trace_index";
 
 impl ProjectDirs {
     pub fn get_trace_index_path() -> Result<PathBuf> {
         let data_dir = dirs::data_dir().context("Couldn't locate data_dir.")?;
-        Ok(data_dir.join(TRACE_INDEX_PATH))
+        Ok(data_dir.join(APP_PATH).join(TRACE_INDEX_PATH))
     }
 }
