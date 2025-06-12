6561f1b0e10c7fb49fd8cc17653840dc4c9479ee
Sangit Manandhar <rux12@icloud.com>
2025-06-12 19:42:33 -0400

    chore: update project_dirs

--- crates/umem_search/src/project_dirs.rs
+++ crates/umem_search/src/project_dirs.rs
@@ -1,14 +1,17 @@
 use anyhow::{Context, Result};
 use std::path::PathBuf;
 
-pub struct ProjectDirs;
-
-const APP_PATH: &str = "umem";
+const APP_NAME: &str = "umem";
 const TRACE_INDEX_PATH: &str = "trace_index";
+
+pub struct ProjectDirs;
 
 impl ProjectDirs {
     pub fn get_trace_index_path() -> Result<PathBuf> {
-        let data_dir = dirs::data_dir().context("Couldn't locate data_dir.")?;
-        Ok(data_dir.join(APP_PATH).join(TRACE_INDEX_PATH))
+        Ok(Self::get_dirs()?.data_dir().join(TRACE_INDEX_PATH))
+    }
+
+    fn get_dirs() -> Result<directories::ProjectDirs> {
+        directories::ProjectDirs::from("", "", APP_NAME).context("cannot get project_dirs for")
     }
 }
