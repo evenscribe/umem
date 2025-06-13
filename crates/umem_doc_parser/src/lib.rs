60ec508e1701b06248056ee729cb45d672560dff
Sangit Manandhar <rux12@icloud.com>
2025-06-12 23:59:07 -0400

    feat: add lazy_static

--- crates/umem_doc_parser/src/lib.rs
+++ crates/umem_doc_parser/src/lib.rs
@@ -1,23 +1,25 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use extractous::{PdfParserConfig, TesseractOcrConfig};
 use std::path::Path;
+
+lazy_static::lazy_static! {
+    static ref EXTRACTOR: extractous::Extractor = {
+        extractous::Extractor::new()
+            .set_ocr_config(TesseractOcrConfig::new())
+            .set_pdf_config(PdfParserConfig::new())
+    };
+}
 
 pub struct Extractor;
 
 impl Extractor {
     pub fn file_to_string<P: AsRef<Path>>(path: P) -> Result<String> {
-        Ok(extractous::Extractor::new()
-            .set_ocr_config(TesseractOcrConfig::new())
-            .set_pdf_config(PdfParserConfig::new())
-            .extract_file_to_string(path.as_ref().to_str().context("path to str thing failed")?)?
+        Ok(EXTRACTOR
+            .extract_file_to_string(path.as_ref().to_string_lossy().as_ref())?
             .0)
     }
 
     pub fn url_to_string(url: String) -> Result<String> {
-        Ok(extractous::Extractor::new()
-            .set_ocr_config(TesseractOcrConfig::new())
-            .set_pdf_config(PdfParserConfig::new())
-            .extract_url_to_string(&url)?
-            .0)
+        Ok(EXTRACTOR.extract_url_to_string(&url)?.0)
     }
 }
