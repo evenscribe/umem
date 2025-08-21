3b0e15c667d4c864ff5f5b6892d5d37d7bf4b895
Sangit Manandhar <rux12@icloud.com>
2025-08-21 01:07:19 -0400

    feat: add get_memory_by_user_id and get_memory_by_query

--- crates/umem_mcp/src/service.rs
+++ crates/umem_mcp/src/service.rs
@@ -1,5 +1,6 @@
+use crate::USER_ID_HEADER;
 use anyhow::Result;
-use axum::http::{self, request::Parts};
+use axum::http::request::Parts;
 use rmcp::{
     handler::server::{
         router::tool::ToolRouter,
@@ -8,20 +9,33 @@ use rmcp::{
     model::{ErrorData as McpError, *},
     schemars, tool, tool_handler, tool_router,
 };
-use tracing::{debug, info};
+use tracing::debug;
 use umem_controller::MemoryController;
 use umem_proto_generated::generated;
 
-use crate::USER_ID_HEADER;
+#[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
+pub struct AddMemoryRequest {
+    pub text: String,
+}
 
 #[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
-pub struct AddMemoryRequst {
-    pub text: String,
+pub struct GetMemoriesByQueryRequest {
+    pub query: String,
 }
 
 #[derive(Clone, Default)]
 pub struct McpService {
     tool_router: ToolRouter<Self>,
+}
+
+fn extract_user_id(parts: Parts) -> String {
+    parts
+        .headers
+        .get(USER_ID_HEADER)
+        .expect("Missing user ID header")
+        .to_str()
+        .expect("Invalid user ID header value")
+        .to_owned()
 }
 
 impl McpService {
@@ -46,17 +60,10 @@ impl McpService {
     async fn add_memory(
         &self,
         Extension(parts): Extension<Parts>,
-        Parameters(AddMemoryRequst { text }): Parameters<AddMemoryRequst>,
+        Parameters(AddMemoryRequest { text }): Parameters<AddMemoryRequest>,
     ) -> Result<CallToolResult, McpError> {
         debug!("add_memory tool called with text: {}", text);
-
-        let user_id: String = parts
-            .headers
-            .get(USER_ID_HEADER)
-            .and_then(|v| v.to_str().ok())
-            .unwrap()
-            .into();
-
+        let user_id = extract_user_id(parts);
         if text.is_empty() {
             return Err(McpError::new(
                 ErrorCode::INVALID_REQUEST,
@@ -64,7 +71,6 @@ impl McpService {
                 None,
             ));
         }
-
         let _ = MemoryController::add_memory(generated::Memory {
             user_id,
             content: text,
@@ -73,6 +79,58 @@ impl McpService {
         .await;
 
         Ok(CallToolResult::success(vec![]))
+    }
+
+    #[tool(
+        name = "get_memory",
+        description = "Get all memories for the current user."
+    )]
+    async fn get_memory(
+        &self,
+        Extension(parts): Extension<Parts>,
+    ) -> Result<CallToolResult, McpError> {
+        let parameters = generated::GetMemoriesByUserIdParameters {
+            user_id: extract_user_id(parts),
+        };
+        let memory_bulk: String = MemoryController::get_memories_by_user_id(parameters)
+            .await
+            .unwrap()
+            .memories
+            .iter()
+            .map(|mem| serde_json::to_string(mem).unwrap())
+            .collect::<Vec<String>>()
+            .join("\n");
+        Ok(CallToolResult::success(vec![Annotated::new(
+            RawContent::Text(RawTextContent { text: memory_bulk }),
+            None,
+        )]))
+    }
+
+    #[tool(
+        name = "get_memory_by_query",
+        description = "Get memories for the current user related to a query."
+    )]
+    async fn get_memory_by_query(
+        &self,
+        Extension(parts): Extension<Parts>,
+        Parameters(GetMemoriesByQueryRequest { query }): Parameters<GetMemoriesByQueryRequest>,
+    ) -> Result<CallToolResult, McpError> {
+        let parameters = generated::GetMemoriesByQueryParameters {
+            user_id: extract_user_id(parts),
+            query,
+        };
+        let memory_bulk: String = MemoryController::get_memories_by_query(parameters)
+            .await
+            .unwrap()
+            .memories
+            .iter()
+            .map(|mem| serde_json::to_string(mem).unwrap())
+            .collect::<Vec<String>>()
+            .join("\n");
+        Ok(CallToolResult::success(vec![Annotated::new(
+            RawContent::Text(RawTextContent { text: memory_bulk }),
+            None,
+        )]))
     }
 }
 
